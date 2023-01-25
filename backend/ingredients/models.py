442a15ddcc9fce49d87bb691c5b2b561bce86810
Александра Карабутова <karabutova.alexa@yandex.ru>
2023-01-25 19:25:24 +0300

    First commit

---
+++ backend/ingredients/models.py
@@ -0,0 +1,32 @@
+from django.conf import settings
+from django.db import models
+
+
+class Ingredient(models.Model):
+    name = models.CharField(
+        "Название",
+        max_length=settings.NAME_MAX_LENGTH,
+        help_text="Введите название",
+    )
+    measurement_unit = models.CharField(
+        "Единица измерения",
+        max_length=settings.MEASUREMENT_UNIT_MAX_LENGTH,
+        help_text="Введите единицу измерения",
+    )
+
+    class Meta:
+        verbose_name = "Ингредиент"
+        verbose_name_plural = "Ингредиенты"
+        ordering = (
+            "name",
+            "measurement_unit",
+        )
+        constraints = [
+            models.UniqueConstraint(
+                name="unique_ingridient",
+                fields=["name", "measurement_unit"],
+            ),
+        ]
+
+    def __str__(self) -> str:
+        return f"{self.name} ({self.measurement_unit})"
---
+++ backend/tags/admin.py
@@ -0,0 +1,25 @@
+from django.conf import settings
+from django.contrib import admin
+
+from .models import Tag
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = (
+        "pk",
+        "name",
+        "color",
+        "slug",
+    )
+    list_editable = (
+        "name",
+        "color",
+        "slug",
+    )
+    search_fields = (
+        "name",
+        "slug",
+    )
+    list_filter = ("name",)
+    empty_value_display = settings.ADMIN_MODEL_EMPTY_VALUE
