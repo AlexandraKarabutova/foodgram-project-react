5fcb77acb14db3745bcf7e9be967baf268af38c4
Александра Карабутова <karabutova.alexa@yandex.ru>
2023-01-30 15:04:25 +0300

    debug true

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -13,7 +13,7 @@ DB_PORT = os.getenv("DB_PORT", default="5432")
 # SECRET_KEY = os.getenv("SECRET_KEY")
 SECRET_KEY = get_random_secret_key()
 
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = os.getenv(
     "HOSTS", default="*"
